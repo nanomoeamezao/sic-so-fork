e0bfa1556967b2e96490f324dc4ec0502a35597c
neo <neo@xd.lmao>
2025-01-22 18:05:24 +0300

    init
    fix: bump to 7 officers
    
    fix: rm jar

---
+++ src/second_in_command/a9kSCModPlugin.kt
@@ -0,0 +1,36 @@
+package second_in_command
+
+import com.fs.starfarer.api.BaseModPlugin
+import com.fs.starfarer.api.Global
+import com.fs.starfarer.api.impl.campaign.ids.Abilities
+import lunalib.lunaDebug.LunaDebug
+import lunalib.lunaSettings.LunaSettings
+import second_in_command.SCUtils.getPlayerData
+import second_in_command.SCData
+import second_in_command.misc.NPCFleetInflater
+import second_in_command.misc.SCSettings
+import second_in_command.misc.a9kSCSettings
+import second_in_command.misc.snippets.AddAllOfficersSnippet
+import second_in_command.misc.snippets.AddXPToOfficersSnippet
+import second_in_command.scripts.*
+import second_in_command.specs.SCOfficer
+import second_in_command.specs.SCSpecStore
+import java.lang.Exception
+
+class a9kSCModPlugin : BaseModPlugin() {
+
+    override fun onApplicationLoad() {
+        LunaSettings.addSettingsListener(a9kSCSettings())
+    }
+
+    override fun onGameLoad(newGame: Boolean) {
+        if (getPlayerData().getAllActiveOfficers().size <4) {
+            getPlayerData().activeOfficers.add(null)
+        }
+
+            /* if (!Global.getSector().memoryWithoutUpdate.getBoolean("\$fourExecs")) {
+                 activeOfficers.add(null)
+                 Global.getSector().memoryWithoutUpdate.set("\$fourExecs", true)
+             }*/
+    }
+}
---
+++ src/second_in_command/misc/a9kSCSettings.kt
@@ -0,0 +1,65 @@
+package second_in_command.misc
+
+import com.fs.starfarer.api.Global
+import lunalib.lunaSettings.LunaSettings
+import lunalib.lunaSettings.LunaSettingsListener
+import second_in_command.SCUtils
+
+class a9kSCSettings : LunaSettingsListener {
+
+
+    companion object {
+        var MOD_ID = "second_in_command_addon"
+
+
+        var maxLevel = LunaSettings.getInt(MOD_ID, "sc_officerMaxLevel")!!
+
+        var crPenalty = LunaSettings.getFloat(MOD_ID, "sc_crPenalty")!!
+
+        var autoPoints = LunaSettings.getFloat(MOD_ID, "sc_autoPoints")!!
+
+        var xpPerLevel = listOf<Float>(
+           /* 0f, //LV0
+            75000f, //LV1
+            150000f, //LV2
+            300000f, //LV3
+            600000f, //LV4*/
+
+            0f, //LV0
+            75000f, //LV1
+            150000f, //LV2
+            300000f, //LV3
+            450000f, //LV4
+            600000f, //LV5
+            750000f, //LV6
+            900000f, //LV7
+            1050000f, //LV8
+            1300000f, //LV9
+
+          /*  0f, //LV0
+            80000f, //LV1
+            160000f, //LV2
+            480000f, //LV3
+            1440000f, //LV4*/
+        )
+    }
+
+    init {
+        applySettings()
+    }
+
+    override fun settingsChanged(modID: String) {
+        /*if (modID == SCUtils.MOD_ID) {
+            applySettings()
+        }*/
+        applySettings()
+    }
+
+    fun applySettings() {
+        crPenalty = LunaSettings.getFloat(MOD_ID, "sc_crPenalty")!!
+        maxLevel = LunaSettings.getInt(MOD_ID, "sc_officerMaxLevel")!!
+        autoPoints = LunaSettings.getFloat(MOD_ID, "sc_autoPoints")!!
+    }
+
+
+}
